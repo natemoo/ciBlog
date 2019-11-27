798f838410cea20b9085df1a701d359634482bc9
natemoo <nathanandrewmoore@gmail.com>
2019-11-27 12:17:02 -0700

    Updated header

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,5 +1,19 @@
 <html>
 <head>
   <title>ciBlog</title>
+  <link rel="stylesheet" href="https://bootswatch.com/4/flatly/bootstrap.min.css">
 </head>
 <body>
+<nav class="navbar navbar-inverse">
+  <div class="container">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">ciBlog</a>
+    </div>
+    <div id="navbar">
+      <ul class="nav navbar-nav">
+        <li><a href="/">Home</a></li>
+        <li><a href="/about">About</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
